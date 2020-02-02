dd790c10268f07fc6cea3dca2a38d9661ddffb37
Ruchit Motiwala <ruchitunicom21@gmail.com>
2020-02-02 19:17:49 +0530

    Changes done for inserting person object

--- person-entities/src/test/java/com/mycompany/app/personentities/PersonEntitiesApplicationTests.java
+++ person-entities/src/test/java/com/mycompany/app/personentities/PersonEntitiesApplicationTests.java
@@ -3,12 +3,16 @@ package com.mycompany.app.personentities;
 
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/*
 @SpringBootTest
 class PersonEntitiesApplicationTests {
 
+	public PersonEntitiesApplicationTests(){
+
+	}
 	@Test
 	void contextLoads() {
 	}
 
 }
+*/
